402ce21e91891eda6e202c1e58f0197ee5f5b60d
sw2park <pswoo990803@gmail.com>
2024-10-02 17:30:04 +0900

    24.10.02 main back 진행중

--- src/main/java/com/reviewfinder/movie/MovieFrontController.java
+++ src/main/java/com/reviewfinder/movie/MovieFrontController.java
@@ -32,7 +32,7 @@ public class MovieFrontController extends HttpServlet{
 		}else if(requestURI.equals("/movie/InsertMovieDB.mv")) {
 			forward = new InsertMovieAction().execute(request, response);
 		}else if(requestURI.equals("/movie/InsertBoxoffice.mv")){
-			
+			forward = new InsertBoxofficeAction().execute(request, response);
 		}else if(requestURI.equals("/movie/MovieDetails.mv")) {	//임시
 			forward = new MovieDetails().execute(request, response);
 		}else if(requestURI.equals("/movie/InsertMovieToGenre.mv")) {
