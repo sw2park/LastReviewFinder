9ad7d580a49e89f0d722b7a0a43b55b31b8f8715
sw2park <pswoo990803@gmail.com>
2024-10-04 16:07:46 +0900

    24.10.04 기존 새 프로젝트 사용하던거 merge (#22)
    DB boxOfficerank에 insert 가능

---
+++ src/main/java/com/reviewfinder/main/GetBoxOfficeAction.java
@@ -0,0 +1,22 @@
+package com.reviewfinder.main;
+
+import com.reviewfinder.action.Action;
+import com.reviewfinder.action.ActionForward;
+import com.reviewfinder.main.dao.MainDAO;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class GetBoxOfficeAction implements Action {
+	@Override
+	public ActionForward execute(HttpServletRequest req, HttpServletResponse resp) {
+		ActionForward forward = new ActionForward();
+		MainDAO mdao = new MainDAO();
+		
+		
+		forward.setRedirect(true);
+		forward.setPath("/main/main.jsp");
+		
+		return forward;
+	}
+}
