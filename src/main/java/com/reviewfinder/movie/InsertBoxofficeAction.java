e91c7adc5f4d8b43988386fef04d20c8772dfddc
sw2park <pswoo990803@gmail.com>
2024-10-04 16:06:23 +0900

    24.10.04 기존 새 프로젝트 사용하던거 merge

--- src/main/java/com/reviewfinder/movie/InsertBoxofficeAction.java
+++ src/main/java/com/reviewfinder/movie/InsertBoxofficeAction.java
@@ -8,6 +8,7 @@ import org.json.simple.parser.ParseException;
 import com.reviewfinder.action.Action;
 import com.reviewfinder.action.ActionForward;
 import com.reviewfinder.api.KobisJson;
+import com.reviewfinder.boxoffice.dao.BoxOfficeDTO;
 import com.reviewfinder.date.DateController;
 import com.reviewfinder.movie.dao.MovieDAO;
 import com.reviewfinder.movie.dao.MovieDTO;
@@ -22,20 +23,29 @@ public class InsertBoxofficeAction implements Action {
 		DateController date = new DateController();
 		KobisJson kobisJson = new KobisJson();
 		HashMap<String, MovieDTO> boxOffice  = null;
+		MovieDAO mdao = new MovieDAO();
+		BoxOfficeDTO bDto = new BoxOfficeDTO();
 		
 		try {
-			// BoxOffice 데이터를 가져옴
-			boxOffice = kobisJson.getBoxOffice(date.getToday());
-			System.out.println(boxOffice.values());
+			for(int i=0; i>-3; i--) {
+				String month = ""+(Integer.parseInt(date.getDay(i))-1);
+				// BoxOffice 데이터를 가져옴
+				boxOffice = kobisJson.getBoxOffice(month);
+				mdao.insertMovieDB(boxOffice);
+				
+				
+				for(int j=1; j<=10; j++) {
+					int movieNum = mdao.selectMovieNum(boxOffice, month, j);
+					
+					bDto.setBoxoffice_date(month);
+					bDto.setMovie_num(movieNum);
+					bDto.setMovie_rank(j);
+					
+					mdao.insertBoxOffice(bDto);
+				}
+			}
 		} catch (ParseException | IOException e) {
 			e.printStackTrace();
-		}
-		
-		if(boxOffice != null) {
-			MovieDAO mdao = new MovieDAO();
-			mdao.insertBoxOffice(boxOffice);
-		} else {
-			System.out.println("boxOffice is null");
 		}
 		
 		forward.setRedirect(true);
